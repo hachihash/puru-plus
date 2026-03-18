7a1c7cd0355f94ee33956acc6705abbe18258728
Naru <8n0lheivgehl@gmail.com>
2026-03-18 23:40:45 +0900

    fix: 型エラーを修正

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  typescript: {
-    // !! 警告 !!
-    // プロジェクトにタイプエラーがあっても、本番ビルドを正常に完了させます。
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    // ビルド時のESLintエラーも無視するようにします
-    ignoreDuringBuilds: true,
-  },
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
 export default nextConfig;
